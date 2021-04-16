94680ce43c711faca550e33245b6035495b5ed48
Natanael Mavare <nata@email.com>
2021-04-16 02:58:02 +0200

    Added Hilda to database

--- adopta_un_amigo (1).sql
+++ adopta_un_amigo (1).sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5ubuntu0.5
+-- version 4.9.5deb2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Feb 21, 2021 at 09:46 PM
--- Server version: 5.7.32-0ubuntu0.18.04.1
--- PHP Version: 7.2.24-0ubuntu0.18.04.7
+-- Generation Time: Apr 16, 2021 at 02:51 AM
+-- Server version: 8.0.23-0ubuntu0.20.04.1
+-- PHP Version: 7.4.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -27,7 +29,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `perros` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `nombre` varchar(255) NOT NULL,
   `edad` varchar(255) NOT NULL,
   `raza` varchar(255) NOT NULL,
@@ -41,7 +43,7 @@ CREATE TABLE `perros` (
 --
 
 INSERT INTO `perros` (`id`, `nombre`, `edad`, `raza`, `genero`, `comportamiento`, `imagen`) VALUES
-(11, 'Hilda', '4 meses', 'Jack Russel', 'Femenino', 'Arrecha', 'https://scontent-dus1-1.xx.fbcdn.net/v/t1.0-1/p148x148/17103321_1239710586120979_5336148633635503619_n.jpg?_nc_cat=110&ccb=3&_nc_sid=1eb0c7&_nc_ohc=r2SAxl80TrkAX-uwFHz&_nc_ht=scontent-dus1-1.xx&tp=6&oh=0cb5c9f17956cdfea9c5aef7ba746248&oe=6052631D'),
+(11, 'Hilda', '4 meses', 'Jack Russel', 'Femenino', 'Arrecha', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuKYG0l9qcd3fsxu4Jqdk67Q0jwp92HFYeBA&usqp=CAU'),
 (12, 'Cornelius', '1 mes y 28 dias', 'Rottweiler', 'Masculino', 'Hiperactivo, Juguetón', 'https://img.bekiamascotas.com/articulos/portada/75000/75312.jpg'),
 (13, 'Max', '3 Meses', 'Pastor Alemán', 'Masculino', 'Juguetón', 'https://www.hola.com/imagenes/estar-bien/20191004150785/pastor-aleman-raza-de-perro-caracteristicas/0-728-57/raza-de-perro-pastor-aleman-m.jpg'),
 (14, 'Pepe', '1 Año', 'Bulldog', 'Masculino', 'Tranquilo', 'https://t1.uc.ltmcdn.com/images/5/3/7/como_saber_si_mi_bulldog_frances_es_puro_50735_600.jpg'),
@@ -67,7 +69,9 @@ ALTER TABLE `perros`
 -- AUTO_INCREMENT for table `perros`
 --
 ALTER TABLE `perros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
